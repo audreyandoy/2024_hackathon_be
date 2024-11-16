98e045959f044ed3f06f03214f1362eb491e76b7
Audrey Andoy <audreylandoy@gmail.com>
2024-11-16 12:50:49 -0800

    setting up hume

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,29 @@
 from flask import Flask
+import asyncio
+from app.voice import main as voice_main
+from dotenv import load_dotenv
+import asyncio
+
 
 
 def create_app():
+    load_dotenv()
     app = Flask(__name__)
 
     from app.routes import routes
     app.register_blueprint(routes)
+
+    @app.before_first_request
+    def start_voice_processing():
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        
+        import threading
+        voice_thread = threading.Thread(
+            target=lambda: loop.run_until_complete(voice_main()),
+            daemon=True
+        )
+        voice_thread.start()
 
     return app
 
@@ -13,4 +31,5 @@ def create_app():
 if __name__ == '__main__':
     app = create_app()
     # app.run(debug=True)
+    asyncio.run(voice_main())
     app.run(port=5000, debug=True, host='0.0.0.0')
